f60ed5322ea147ba2da9d6907cccbcb2f03cb578
Richard Jackson <rpjackson2@gmail.com>
2022-04-28 11:26:17 -0500

    added tests

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 public class Student {
     private long id;
     private String name;
-    private ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> grades;
 
-   Student(long id, String name,ArrayList<Integer> grades) {
+   Student(long id, String name) {
         this.id = id;
         this.name = name;
+        this.grades = new ArrayList<>();
     }
 
     // returns the student's id
@@ -25,10 +26,20 @@ public class Student {
     }
     // adds the given grade to the grades list
     public void addGrade(int grade){
-
+        grades.add(grade);
+//        grades.add(100);
+//        grades.add(80);
+//        grades.add(95);
     }
     // returns the average of the students grades
-    public double getGradeAverage(){
-
+    public double getGradeAverage() {
+       double addy = 0.0;
+//       double avg = 0.0;
+        for (int grade : grades) {
+            addy +=grade;
+        }
+        return addy / grades.size();
     }
+
+    
 }
--- src/test/java/StudentTests.java
+++ src/test/java/StudentTests.java
@@ -3,11 +3,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTests {
-    private Student student;
 
-    @Before
-    public void setup(){
-
-        student = new Student(1,"Richard",ArrayList<Integer> grades);
+//    @Before
+//    public void setup(){
+//        Student student = new Student(1, "Richard");
+//    }
+    @Test
+    public void testIfGradesInitialized(){
+        Student student = new Student(1, "Richard");
+        assertNotNull(student.getGrades());
     }
+@Test
+    public void doesAvgEqualNinetyOnePointSix(){
+    Student student = new Student(1, "Richard");
+    student.addGrade(100);
+    student.addGrade(80);
+    student.addGrade(95);
+        assertEquals(91.6,student.getGradeAverage(),0.1);
+}
 }
